044b7330a2a8eb8e5ef40441797c810340ca3bb5
sadiabinta <sadiaa.kabir@gmail.com>
2023-10-08 10:47:10 -0400

    helmet

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,16 +1,19 @@
 import Arrived from "../Arrived/Arrived";
 import Banner from "../Banner/Banner";
 import Category from "../Category/Category";
-
+import { Helmet } from "react-helmet-async";
 
 const Home = () => {
-    return (
-        <div>
-            <Banner></Banner>
-            <Category></Category>
-            <Arrived></Arrived>
-        </div>
-    );
+  return (
+    <div>
+      <Helmet>
+        <title>Arrosa | Home</title>
+      </Helmet>
+      <Banner></Banner>
+      <Category></Category>
+      <Arrived></Arrived>
+    </div>
+  );
 };
 
 export default Home;
--- src/pages/shared/Navbar/Navbar.jsx
+++ src/pages/shared/Navbar/Navbar.jsx
@@ -1,18 +1,13 @@
+import { Link } from "react-router-dom";
 
 
 const Navbar = () => {
     const navOptions=<>
-    <li><a>Item 1</a></li>
-      <li tabIndex={0}>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
+    <li><Link to='/'>Home</Link></li>
+    <li><Link>All Jewelry</Link></li>
+    <li><Link>My Jewelry</Link></li>
+      <li><Link>Add Jewelry</Link></li>
+      <li><Link>Blogs</Link></li>
     </>
     return (
         <>
